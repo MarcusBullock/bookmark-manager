b98d8341bd9bb86634021d1e6b80be7589ee10cf
Marcus <marcusbullock@hotmail.co.uk>
2016-02-11 12:34:40 +0000

    did some stuff

--- spec/features/user_registration_password_confirmation_spec.rb
+++ spec/features/user_registration_password_confirmation_spec.rb
@@ -3,6 +3,8 @@ feature 'Password confirmation' do
 
   scenario '-> requires password confirmation' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(page).to have_current_path('/register')
+    expect(page).to have_content('Wrong')
   end
 
 end
